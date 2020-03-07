353200e5e71970d58a035094eaa3abca13228acd
Michal Dziadkowiec <michaldziadkowiec@yahoo.pl>
2020-03-07 11:51:21 +0100

    Add basic lambda function

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,20 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = {
+  // for avoiding CORS while developing Netlify Functions locally
+  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
+  developMiddleware: app => {
+    app.use(
+      '/.netlify/functions/',
+      createProxyMiddleware({
+        target: 'http://localhost:9000',
+        pathRewrite: {
+          '/.netlify/functions/': '',
+        },
+      })
+    );
+  },
   siteMetadata: {
     title: `Michał Dziadkowiec - Front-End Developer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
